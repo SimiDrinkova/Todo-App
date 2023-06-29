db15fa19898a6b3bfc0e957a014a1025557abc86
Simi <sdrinkova@devopsgroup.sk>
2023-06-29 09:22:37 +0200

    todo app + added local storage

--- script.js
+++ script.js
@@ -10,17 +10,16 @@ todoButton.addEventListener('click', addTodo)
 let todos = []
 
 
-// funkcia na pridanie noveho todo itemu
+// funkcia na zachytenie udalosti a vstupu uzivatela, vytvorenie item a pushnutie to do "todos"
 function addTodo (event) {
 
-  const items = {
+  const item = {
     id: new Date().getTime(),
     text: todoInput.value,
     complete: false
   }
 
-  todos.unshift(items);
-  
+  todos.unshift(item);
   event.preventDefault()
 
   if (todoInput.value === ""){
@@ -28,42 +27,41 @@ function addTodo (event) {
     return;
   } 
 
-  
-  /* <div class="item">
+  const todoItem = createTodoItem(item)
+  todoList.prepend(todoItem)
+
+  todoInput.value = ""
+  Save()
+}
+
+function createTodoItem(item) {
+
+   /* <div class="item">
 	<input type="checkbox" />
 	<input 
 		type="text" 
 		value="Todo content goes here" 
 		disabled /> */
+
+  const todoItem = document.createElement('div');
+  todoItem.className = 'item';
+
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = item.complete;
+  todoItem.appendChild(checkbox);
+
+  if (item.complete) {
+    todoItem.classList.add("complete");
+  }
+  Save()
   
-    const item = document.createElement('div')
-    item.className = 'item'
-    todoList.prepend(item)
-    //posledny pridany task bude prvy vdaka prepend
-    //todoList.insertBefore(item, todoList.firstChild);
-    
-
-    const checkbox = document.createElement('input')
-    checkbox.type = 'checkbox'
-    checkbox.checked = items.complete
-    item.appendChild(checkbox)
-    
-
-    if (items.complete) {
-      item.classList.add("complete");
-    }
-    Save()
-
-    const input = document.createElement('input')
-    input.type = "text" 
-    input.className = 'todo-text'
-    input.value = items.text
-    input.disabled = true
-    item.appendChild(input)
-    
-
-
-    todoInput.value = ""
+  const input = document.createElement('input');
+  input.type = 'text';
+  input.className = 'todo-text';
+  input.value = item.text;
+  input.disabled = true;
+  todoItem.appendChild(input);
 
 
     /*  <div class="actions">
@@ -71,76 +69,79 @@ function addTodo (event) {
 						<button class="material-icons remove-btn">delete</button>
 					</div>
     */
-   
-    const actions = document.createElement('div')
-    actions.className = 'actions' 
-    item.appendChild(actions)     
 
-    const edit = document.createElement('button')
-    edit.className = 'material-icons'
-    edit.innerText = 'edit'
-    actions.appendChild(edit)
+  const actions = document.createElement('div');
+  actions.className = 'actions';
+  todoItem.appendChild(actions);
 
-    const remove = document.createElement('button')
-    remove.className = 'material-icons remove btn'
-    remove.innerText = 'delete'
-    actions.appendChild(remove)
+  const edit = document.createElement('button');
+  edit.className = 'material-icons';
+  edit.innerText = 'edit';
+  actions.appendChild(edit);
+
+  const remove = document.createElement('button');
+  remove.className = 'material-icons remove btn';
+  remove.innerText = 'delete';
+  actions.appendChild(remove);
 
 
+// EVENTY
+//ak uzivatel klikne na edit, povolim input a moze to prepisat
+  edit.addEventListener('click', () => {
+    input.removeAttribute('disabled');
+    input.focus();
+    Save();
+  });
 
-    //EVENTY
+ //ak uzivatel klikne mimo upraveneho inputu, bude to znova disabled  
+  input.addEventListener('blur', () => {
+    input.setAttribute('disabled', '');
+    Save();
+  });
 
-  //  zachytavam, co uzivatel pise do inputu "your task" 
-  todoInput.addEventListener('input', ()=>{
-    items.text = todoInput.value
-  })  
+  //ak uzivatel klikne delete, vymazem dany element  
+  remove.addEventListener('click', () => {
+    todos = todos.filter(t => t.id !== item.id);
+    todoItem.remove();
+    Save();
+  });
 
-  //ak uzivatel klikne na edit, povolim input a moze to prepisat
-    edit.addEventListener('click', ()=>{
-      input.removeAttribute('disabled')
-      input.focus()
-      Save()
-    })
+  //ak uzivatel klikne checkbox, pridame/odoberieme elementu classu + zaskrtneme hotovy task
+  checkbox.addEventListener('change', () => {
+    item.complete = checkbox.checked;
 
-  //ak uzivatel klikne mimo upraveneho inputu, bude to znova disabled    
-    input.addEventListener('blur', ()=>{
-      input.setAttribute('disabled', '')
-      Save()
-    })
-
-  // ak uzivatel klikne delete, vymazem dany element  
-    remove.addEventListener('click', ()=>{
-      todos = todos.filter(t=>t.id != items.id)
-      item.remove()
-      Save()
-    })
-
-  // ak uzivatel klikne checkbox, pridame/odoberieme elementu classu + zaskrtneme hotovy task  
-    checkbox.addEventListener('change', ()=>{
-      items.complete = checkbox.checked
-
-      if (items.complete) {
-          checkbox.classList.add('complete')
-          input.style.textDecoration = 'line-through';
-      // vsetky hotove tasky sa posunu dole     
-          todoList.appendChild(item)
-      } else {
-        checkbox.classList.remove('complete')
-        input.style.textDecoration = 'none';
-      }
-      Save()
-    })
-
-    function Save () {
-      const saved = JSON.stringify(todos)
-      localStorage.setItem('myTodos', saved)
+    if (item.complete) {
+      checkbox.classList.add('complete');
+      input.style.textDecoration = 'line-through';
+      //vsetky hotove tasky sa posunu dole   
+      todoList.appendChild(todoItem);
+    } else {
+      checkbox.classList.remove('complete');
+      input.style.textDecoration = 'none';
+      todoList.prepend(todoItem);
     }
+    Save();
+  });
+
+  return todoItem;
 }
 
+// LOCAL STORAGE
 
+function DisplayTodo() {
+  const savedTodos = localStorage.getItem('myTodos');
+  if (savedTodos) {
+    todos = JSON.parse(savedTodos);
+    for (let i = 0; i < todos.length; i++) {
+      const todoItem = createTodoItem(todos[i]);
+      todoList.appendChild(todoItem);
+    }
+  }
+}
 
+function Save() {
+  const saved = JSON.stringify(todos);
+  localStorage.setItem('myTodos', saved);
+}
 
-
-
-
-
+DisplayTodo();
