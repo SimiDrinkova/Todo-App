1e40aa1f2efa192e077c9e5f850cf054d7406fce
Simi <sdrinkova@devopsgroup.sk>
2023-08-10 15:40:39 +0200

    live-server install

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5500",
+    baseUrl: "http://127.0.0.1:8080/",
     viewportWidth: 1920,
     viewportHeight: 1080,
     video: false,
